aed8bfe1748fb221cc3fcf62495f0af5948b0ed6
retroinspect <sangachoi2103@gmail.com>
2021-01-25 14:45:52 +0900

    refactor: remove useReducer : to make state-lifing easier

--- src/component/ReviewMoa/AppHeader.js
+++ src/component/ReviewMoa/AppHeader.js
@@ -5,7 +5,7 @@ import React from "react";
 export default function AppHeader({ reviewCount }) {
   return (
     <div className="app-header d-flex">
-      <h1> 리뷰모아 </h1>
+      <h1> 리뷰 편하게 보기 </h1>
       <h2>
         {reviewCount} 개 리뷰
       </h2>
